eafe31c55b6fd2cdd44d9ae055ec1853e8f7e7b6
Liza <kruhlikeliza@gmail.com>
2020-07-05 21:30:19 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const https = require('https');
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/signup.html"));
+
+app.post('/', function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+    var jsonData = JSON.stringify(data);
+
+    var url = 'https://us10.api.mailchimp.com/3.0/lists/33541f9080';
+    var options = {
+        method: "post",
+        auth: "LizKru:6d19d8c4b7964f5a4d01515451e7ac26-us10"
+    };
+
+    const request = https.request(url, options, function(response) {
+        response.on("data", (data) => console.log(JSON.parse(data)))
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(port, () => console.log("Server is running on port " + port));
+
+//6d19d8c4b7964f5a4d01515451e7ac26-us10
+//33541f9080
